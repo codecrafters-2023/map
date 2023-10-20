c594aa7594fdceaa0ccdd30e8ec4cf4f32c474f0
codecrafters-2023 <codecrafters@gurmaanitservices.com>
2023-10-21 03:58:34 +0530

    add email

---
+++ pages/api/sendgrid.js
@@ -0,0 +1,53 @@
+import sendgrid from "@sendgrid/mail";
+import Image from "next/image";
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+const image = "https://plus.unsplash.com/premium_photo-1666323594456-cd1b09b2cbf4?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWFyfGVufDB8fDB8fHww"
+
+async function sendEmail(req, res) {
+  // NOTE: Uncomment the below lines to make the code work
+
+  try {
+    await sendgrid.send({
+      to: req.body.email, // Your email where you'll receive emails
+      from: "codecrafters@gurmaanitservices.com", // your website email address here
+      subject: `${req.body.subject}`,
+      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+
+        <title>The HTML5 Herald</title>
+        <meta name="description" content="The HTML5 Herald">
+        <meta name="author" content="SitePoint">
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+      </head>
+
+      <body>
+        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow:    hidden; font-family: 'helvetica', 'ui-sans';">
+        </div>
+              <div class="container" style="margin-left: 20px;margin-right: 20px;">
+              <h3>You've got a new mail from ${req.body.fullname}, their email is: ✉️${req.body.email} </h3>
+              <div style="font-size: 16px;">
+              <p>Message:</p>
+              <p>${req.body.message}</p>
+              <br>
+              </div>
+              </div>
+      </body>
+      </html>`,
+    });
+  } catch (error) {
+    // console.log(error);
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
+
+  return res.status(200).json({ error: "" });
+}
+
+export default sendEmail;
--- pages/hello.js
+++ pages/hello.js
@@ -1,37 +1,14 @@
-// <!DOCTYPE html>
-// <html>
-//   <head>
-//     <title>Simple Marker</title>
-//     <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8jE3Eu0XmLvBbLjEdQPVfoToAOavqE0U&callback=console.debug&libraries=maps,marker&v=beta">
-//     </script>
-//     <style>
-//       gmp-map {
-//         height: 100%;
-//       }
-
-//       /* Optional: Makes the sample page fill the window. */
-//       html,
-//       body {
-//         height: 100%;
-//         margin: 0;
-//         padding: 0;
-//       }
-//     </style>
-//   </head>
-//   <body>
-//     <gmp-map center="34.0922966003418,-117.43505096435547" zoom="14" map-id="DEMO_MAP_ID">
-//       <gmp-advanced-marker position="34.0922966003418,-117.43505096435547" title="My location">
-//       </gmp-advanced-marker>
-//     </gmp-map>
-//   </body>
-// </html>
-
-
+import ContactUs from '@/components/Contact'
 import React from 'react'
 
 const hello = () => {
   return (
-    <div>hello</div>
+    <div className="">
+
+    <main className="">
+      <ContactUs />
+    </main>
+  </div>
   )
 }
 
